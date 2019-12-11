a7d870d966b650c8f37b5bc986e89d2eb93c7317
Doug <douglaspinheiro@id.uff.br>
2019-12-11 17:08:29 -0300

    config gulp connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    connect = require('gulp-connect-php'),
+    browserSync = require('browser-sync');
  
-gulp.task('server', function() {
-  browserSync.init({
-     server: {
-   proxy: "localhost/formCurriculo/src/",
-      ghostMode: false, 
-        baseDir: 'src'
-     },
-  })
+gulp.task('connect-sync', function() {
+  connect.server({}, function (){
+    browserSync({
+      proxy: '127.0.0.1:8000',
+      files: [
+        "**/**.php",
+        "**/**.css",
+        "**/**.js"
+      ]
+    });
+  });
+ 
+  gulp.watch('src/**/*.php').on('change', function () {
+    browserSync.reload();
+  });
 
-  gulp.watch("src/*.css").on('change', browserSync.reload);
-  gulp.watch("src/*.php").on('change', browserSync.reload);
-})
+  gulp.watch('src/**/*.css').on('change', function () {
+    browserSync.reload();
+  });
+});
