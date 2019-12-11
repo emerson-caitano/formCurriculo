ae00fb0592fafb8b653d6c618f93213ecf4ec78e
Emerson Caitano <16889644+emerson-caitano@users.noreply.github.com>
2019-12-11 16:17:14 -0300

    bug html > php

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var browserSync = require('browser-sync').create();
 gulp.task('server', function() {
   browserSync.init({
      server: {
+   proxy: "localhost/formCurriculo/src/",
+      ghostMode: false, 
         baseDir: 'src'
      },
   })
 
   gulp.watch("src/*.css").on('change', browserSync.reload);
-  gulp.watch("src/*.html").on('change', browserSync.reload);
+  gulp.watch("src/*.php").on('change', browserSync.reload);
 })
