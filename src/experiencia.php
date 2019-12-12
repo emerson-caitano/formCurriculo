67deb7bc7804420c23c1ccedba721867101eb0a5
Emerson Caitano <16889644+emerson-caitano@users.noreply.github.com>
2019-12-12 20:43:37 -0300

    desenvolvendo layout pag experiencia

--- src/experiencia.php
+++ src/experiencia.php
@@ -1,14 +1,14 @@
 <?php require_once("cabecalho/index.php"); ?>
 
 <div class="container"> <!-- empresa, cargo, dataInicial, dataFinal, localizacao, funcoes -->
-    <h1>Experiência</h1>
+    <h1>Experiência Profissional</h1>
     <form>
     <div class="row">
         <div class="form-group col-md-9">
-            <label for="empresa">Experiência Profissional</label>
+            <label for="empresa">Empresa</label>
             <input type="text" class="form-control" id="empresa">
         </div>
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-2">
             <label for="dataInicial">Data Inicio</label>
             <input type="text" class="form-control" id="dataInicial" placeholder="00/00/0000">
         </div>
@@ -28,7 +28,7 @@
                 <option>Recife</option>
             </select>
         </div>
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-2">
             <label for="dataFinal">Data Final</label>
             <input type="text" class="form-control" id="dataFinal" placeholder="00/00/0000">
         </div>
@@ -49,22 +49,34 @@
 <table class="table">
     <thead>
         <tr>
-        <th scope="col">Certificados</th>
-        <th scope="col">Descrição</th>
+        <th scope="col">Empresa</th>
+        <th scope="col">Cargo</th>
+        <th scope="col">Localização</th>
+        <th scope="col">Data Inicio</th>
+        <th scope="col">Data Final</th>
         </tr>
     </thead>
     <tbody>
     <tr>
+        <td>Microsoft Corporate</td>
+        <td>Owner</td>
         <td>Mark</td>
-        <td>Otto</td>
+        <td>00/00/0000</td>
+        <td>00/00/0000</td>
     </tr>
     <tr>
-        <td>Jacob</td>
-        <td>Thornton</td>
+        <td>Dougkariok System </td>
+        <td>Mark</td>
+        <td>Rio de Janeiro</td>
+        <td>00/00/0000</td>
+        <td>00/00/0000</td>
     </tr>
     <tr>
         <td>Larry</td>
-        <td>the Bird</td>
+        <td>Mark</td>
+        <td>Rio Grande do Sul</td>
+        <td>00/00/0000</td>
+        <td>00/00/0000</td>
     </tr>
   </tbody>
 </table>
