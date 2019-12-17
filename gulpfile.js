6cfdc6b89a8fe05721d4eaf3e4d2a7874927d5cc
Emerson Caitano <16889644+emerson-caitano@users.noreply.github.com>
2019-12-17 19:12:57 -0300

    alterando localhost

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
 gulp.task('connect-sync', function() {
   connect.server({}, function (){
     browserSync({
-      proxy: '127.0.0.1/formCurriculo/src/',
+      proxy: '127.0.0.1',
       files: [
         "**/**.php",
         "**/**.css",
