c99229e244f7d9f41c3791a52bfd3eb8e7084dc2
Emerson Davi Caitano <4885183-emersoncaitano@users.noreply.gitlab.com>
2020-01-26 21:51:07 -0300

    dao update

--- src/dao/db.php
+++ src/dao/db.php
@@ -1,7 +1,15 @@
 <?php 
 $mysqli = new mysqli("localhost", "root", "12345", "curriculo");
 
+/* change character set to utf8 */
+if (!$mysqli->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $mysqli->error);
+    exit();
+}
+
+
 if ($mysqli -> connect_errno) {
     echo "Failed to connecto to MySQL: " . $mysqli -> connect_error;
+    printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
