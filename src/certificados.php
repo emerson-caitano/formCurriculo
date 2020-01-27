aa8f03ee673c30048d72c2d2607a3fdefa7ac51f
Emerson Davi Caitano <4885183-emersoncaitano@users.noreply.gitlab.com>
2020-01-26 21:51:47 -0300

    certificado while $row

--- src/certificados.php
+++ src/certificados.php
@@ -16,7 +16,7 @@
         ) values (
             '{$_GET["certificado"]}', '{$_GET["descricao"]}', '{$_GET["link"]}', 1
         )";
-        echo ($sql);
+        // echo ($sql);
         if ($mysqli->query($sql) === TRUE) {
             $mensagem = "Salvo com sucesso";
             $certificado=[
@@ -29,20 +29,15 @@
             $mensagem = "Erro ao salvar";
         }
     } else{
-        $result = $mysqli->query("select * from certificados where usuario=1") or die(mysqli_error($cx));
+        $result = $mysqli->query("select * from certificados where usuario=1");
         // var_dump($result);
-        // echo("<br>");
-        $row = $result->fetch_assoc();
+        // $row = $result->fetch_assoc();
         // var_dump($row);
         $certificados=[
             "certificado" => "",
             "descricao" => "",
             "link" => "",
         ];
-    
-        if ($row != NULL){
-            $certificado = $row;
-        }
     }
 ?>
 
@@ -57,51 +52,44 @@
     <h1>Certificados</h1>
     <form>
 
-    <div class="row">
-        <div class="form-group col-md-6">
-            <label for="descricao">Descrição</label>
-            <input type="text" class="form-control" id="descricao" value="<?=$certificado['descricao'];?>">
+        <div class="row">
+            <div class="form-group col-md-6">
+                <label for="descricao">Descrição</label>
+                <input type="text" class="form-control" id="descricao" value="<?=$certificado['descricao'];?>">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="link">Link</label>
+                <input type="text" class="form-control" id="link" placeholder="Link do certificado http://www.curso.com/certificado/123455" value="<?=$certificado['link'];?>">
+            </div>
         </div>
-        <div class="form-group col-md-6">
-            <label for="link">Link</label>
-            <input type="text" class="form-control" id="link" placeholder="Link do certificado http://www.curso.com/certificado/123455" value="<?=$certificado['link'];?>">
+        <div class="row">
+            <div class="col-auto">
+                <button type="reset" class="btn btn-primary mb-2">Novo</button>
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-2">Salvar</button>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-auto">
-            <button type="reset" class="btn btn-primary mb-2">Novo</button>
-        </div>
-        <div class="col-auto">
-            <button type="submit" class="btn btn-primary mb-2">Salvar</button>
-        </div>
-    </div>
     </form>
     <p><?=$mensagem;?></p>
 
-</br>
-<table class="table">
-    <thead>
-        <tr>
-        <th scope="col">Descrição</th>
-        <th scope="col">Link</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td><?php echo $certificado['descricao']; ?></td>
-            <td><?php echo $certificado['link']; ?></td>
-        </tr>
-    </tbody>
-</table>
-
-
-<!-- < ?php
-var_dump($certificado);
-if ($row != NULL){
-            $certificado = $row;
-        }
-?> -->
-
+    <br>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Descrição</th>
+                <th scope="col">Link</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($row = mysqli_fetch_assoc($result)) {?>
+                <tr>
+                    <td><?=$row["descricao"];?></td>
+                    <td><?=$row["link"];?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
 
 <?php require_once("footer/footer.php"); ?>
